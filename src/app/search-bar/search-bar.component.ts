cfc9e0ce09c9e0ee09df44ad8b9ceed3c40d4c29
Hub3rt65@gmail.com <Hubert65@gmail.com>
2022-09-28 07:07:02 +0200

    commit-3

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
   term = '';
 
   constructor() { }
@@ -15,4 +16,9 @@ export class SearchBarComponent implements OnInit {
   // onInput(value: string) {
   //   this.term = value;
   // }
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
