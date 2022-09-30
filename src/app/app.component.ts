010dd0df2066f60f06a68ee77ed54680c9f9b8e8
Hub3rt65@gmail.com <Hubert65@gmail.com>
2022-09-30 10:24:15 +0200

    commit-6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { WikipediaService } from './wikipedia.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   pages = [];
